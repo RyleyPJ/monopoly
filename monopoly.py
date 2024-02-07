9eca9ad99cba25cfbcb8fd83fbfa2ad80b3ab8cc
Ryley <jonesryley07@gmail.com>
2024-02-07 00:31:29 +0000

    First section of the code but still WIP

---
+++ monopoly.py
@@ -0,0 +1,17 @@
+import random 
+    
+# Board and property code 
+
+class Board:
+    def __init__(self, board):
+        self.board = board 
+        
+class Dice: 
+    def __init__(self, dice, board):
+        self.dice = dice 
+        self.board = board
+        
+        dice = random.math()
+        
+        
+            
